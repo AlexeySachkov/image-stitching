f22167748df326bc8a9f6fade1964526441090f8
Alexey Sachkov <sachkov2011@gmail.com>
2019-04-13 22:28:24 +0300

    Add main loop to stich several images in one invocation
    Actually, more or less adequate result can be achieved only
    when using two images

--- main.cpp
+++ main.cpp
@@ -23,115 +23,122 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-  vector<Point2f> corners1;
-  vector<Point2f> rectangle1;
-  Mat result1;
-  Mat image1 = imread(argv[1]);
+  // prepare base image
+  vector<Point2f> corners_base;
+  vector<Point2f> rectangle_base;
+  Mat base;
+  Mat image = imread(opts.file_paths.front());
 
-  if (!projectToTheFloor(image1, Size(3, 4), result1, rectangle1, corners1)) {
-    cout << "Failed to handle first image!" << endl;
+  if (!projectToTheFloor(image, Size(3, 4), base, rectangle_base, corners_base)) {
+    cout << "Failed to handle base image!" << endl;
     return -1;
   }
 
-  displayResult("image1", image1);
-  displayResult("result1", result1, true);
-  //imwrite("result1.jpg", result1);
+  displayResult("image", image);
+  displayResult("base", base, true);
+  //imwrite("base.jpg", base);
 
-  vector<Point2f> corners2;
-  vector<Point2f> rectangle2;
-  Mat result2;
-  Mat image2 = imread(argv[2]);
+  for (size_t i = 1; i < opts.file_paths.size(); ++i) {
+    // stitch the next image to the base image
+    vector<Point2f> corners;
+    vector<Point2f> rectangle;
+    Mat temp;
+    Mat next_image = imread(opts.file_paths[i]);
 
-  if (!projectToTheFloor(image2, Size(3, 4), result2, rectangle2, corners2)) {
-    cout << "Failed to handle second image!" << endl;
-    return -1;
+    if (!projectToTheFloor(next_image, Size(3, 4), temp, rectangle, corners)) {
+      cout << "Failed to handle second image!" << endl;
+      return -1;
+    }
+
+    displayResult("next_image", next_image);
+    displayResult("temp", temp, true);
+    //imwrite("next_image.jpg", temp);
+
+    // TODO: automatically detect rotation
+    vector<Point2f> rotatedRect2;
+    if (false) {
+      rotatedRect2.push_back(rectangle[3]);
+      rotatedRect2.push_back(rectangle[0]);
+      rotatedRect2.push_back(rectangle[1]);
+      rotatedRect2.push_back(rectangle[2]);
+    } else {
+      rotatedRect2 = rectangle;
+    }
+
+    Mat preH = findHomography(Mat(rotatedRect2), Mat(rectangle_base), CV_RANSAC);
+    vector<Point2f> secondCorners = extractCorners(temp);
+    Mat tcorners;
+    perspectiveTransform(Mat(secondCorners), tcorners, preH);
+    vector<Point2f> newSecondCorners = (vector<Point2f>(tcorners));
+    corners_info_t ci(newSecondCorners);
+
+    vector<Point2f> firstCorners = extractCorners(base);
+    corners_info_t fci(firstCorners);
+
+    float minX = fmin(ci.minX, fci.minX);
+    float maxX = fmax(ci.maxX, fci.maxX);
+    float minY = fmin(ci.minY, fci.minY);
+    float maxY = fmax(ci.maxY, fci.maxY);
+
+    float height = maxY - minY;
+    float width = maxX - minX;
+
+    Mat merged(Size(width, height), base.type());
+
+    Rect roi1(Point(fabs(minX), fabs(minY)), Size(fci.width, fci.height));
+    Rect roi2(Point(0, 0), Size(ci.width, ci.height));
+
+    vector<Point2f> shiftedRect1 = rectangle_base;
+    for (auto &P : shiftedRect1) {
+      P.x += fabs(minX);
+      P.y += fabs(minY);
+    }
+
+    Mat H = findHomography(Mat(rotatedRect2), Mat(shiftedRect1), CV_RANSAC);
+
+    Mat rotated2;
+    warpPerspective(temp, rotated2, H, Size(ci.width, ci.height));
+
+    Mat destRoi1 = merged(roi1);
+    Mat destRoi2 = merged(roi2);
+
+    Mat mask1(Size(fci.width, fci.height), base.type());
+    vector<Point> cm1;
+    for (auto &P : corners_base) {
+      cm1.push_back(P);
+    }
+    fillConvexPoly(mask1, cm1, Scalar(255, 255, 255));
+
+    Mat mask2(Size(ci.width, ci.height), base.type());
+    vector<Point2f> rotatedCorners2;
+    if (false) {
+      rotatedCorners2.push_back(corners[3]);
+      rotatedCorners2.push_back(corners[0]);
+      rotatedCorners2.push_back(corners[1]);
+      rotatedCorners2.push_back(corners[2]);
+    } else {
+      rotatedCorners2 = corners;
+    }
+
+    Mat tcorners2;
+    perspectiveTransform(Mat(rotatedCorners2), tcorners2, H);
+    vector<Point2f> newCorners2 = (vector<Point2f>)tcorners2;
+    vector<Point> cm2;
+    for (auto &P : newCorners2) {
+      cm2.push_back(P);
+    }
+    fillConvexPoly(mask2, cm2, Scalar(255, 255, 255));
+
+    base.copyTo(destRoi1, mask1);
+    rotated2.copyTo(destRoi2, mask2);
+
+    displayResult("final", merged, true);
+    //imwrite("merged.jpg", merged);
+
+    // Update the base image
+    base = merged;
+    // TODO: recalculate base corners and rectangle
   }
-
-  displayResult("image2", image2);
-  displayResult("result2", result2, true);
-  //imwrite("result2.jpg", result2);
-
-  vector<Point2f> rotatedRect2;
-  if (false) {
-    rotatedRect2.push_back(rectangle2[3]);
-    rotatedRect2.push_back(rectangle2[0]);
-    rotatedRect2.push_back(rectangle2[1]);
-    rotatedRect2.push_back(rectangle2[2]);
-  } else {
-    rotatedRect2 = rectangle2;
-  }
-
-  Mat preH = findHomography(Mat(rotatedRect2), Mat(rectangle1), CV_RANSAC);
-  vector<Point2f> secondCorners = extractCorners(result2);
-  Mat tcorners;
-  perspectiveTransform(Mat(secondCorners), tcorners, preH);
-  vector<Point2f> newSecondCorners = (vector<Point2f>(tcorners));
-  corners_info_t ci(newSecondCorners);
-
-  vector<Point2f> firstCorners = extractCorners(result1);
-  corners_info_t fci(firstCorners);
-
-  float minX = fmin(ci.minX, fci.minX);
-  float maxX = fmax(ci.maxX, fci.maxX);
-  float minY = fmin(ci.minY, fci.minY);
-  float maxY = fmax(ci.maxY, fci.maxY);
-
-  float height = maxY - minY;
-  float width = maxX - minX;
-
-  Mat result(Size(width, height), image1.type());
-
-  Rect roi1(Point(fabs(minX), fabs(minY)), Size(fci.width, fci.height));
-  Rect roi2(Point(0, 0), Size(ci.width, ci.height));
-
-  vector<Point2f> shiftedRect1 = rectangle1;
-  for (auto &P : shiftedRect1) {
-    P.x += fabs(minX);
-    P.y += fabs(minY);
-  }
-
-  Mat H = findHomography(Mat(rotatedRect2), Mat(shiftedRect1), CV_RANSAC);
-
-  Mat rotated2;
-  warpPerspective(result2, rotated2, H, Size(ci.width, ci.height));
-
-  Mat destRoi1 = result(roi1);
-  Mat destRoi2 = result(roi2);
-
-  Mat mask1(Size(fci.width, fci.height), image1.type());
-  vector<Point> cm1;
-  for (auto &P : corners1) {
-    cm1.push_back(P);
-  }
-  cout << "cm1: " << cm1 << endl;
-  fillConvexPoly(mask1, cm1, Scalar(255, 255, 255));
-
-  Mat mask2(Size(ci.width, ci.height), image2.type());
-  vector<Point2f> rotatedCorners2;
-  if (false) {
-    rotatedCorners2.push_back(corners2[3]);
-    rotatedCorners2.push_back(corners2[0]);
-    rotatedCorners2.push_back(corners2[1]);
-    rotatedCorners2.push_back(corners2[2]);
-  } else {
-    rotatedCorners2 = corners2;
-  }
-
-  Mat tcorners2;
-  perspectiveTransform(Mat(rotatedCorners2), tcorners2, H);
-  vector<Point2f> newCorners2 = (vector<Point2f>)tcorners2;
-  vector<Point> cm2;
-  for (auto &P : newCorners2) {
-    cm2.push_back(P);
-  }
-  cout << "cm2: " << cm2 << endl;
-  fillConvexPoly(mask2, cm2, Scalar(255, 255, 255));
-
-  result1.copyTo(destRoi1, mask1);
-  rotated2.copyTo(destRoi2, mask2);
-
-  displayResult("final", result, true);
-  //imwrite("result.jpg", result);
 
   return 0;
 }
